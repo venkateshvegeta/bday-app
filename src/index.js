5479159e7f310093dccbfdd7c3d1b5c17ad79587
Venkatesh <venkateshv455@gmail.com>
2020-04-29 21:54:59 +0530

    Bday app initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const port = process.env.PORT || 3000
+const publicDirPath = path.join(__dirname,'../public')
+app.use(express.static(publicDirPath))
+app.set('views',path.join(__dirname,'../public/html'))
+app.set('view engine', 'hbs')
+app.get("/",(req,res) => {
+    res.render('index',{})
+})
+
+app.listen(port,() => {
+    console.log("Server is up on Port: ",port)
+    
+})
